17f0ebbbc9f20db91dc089bd564b2b63c9ed8fd6
odinG5 <57181291+odinG5@users.noreply.github.com>
2020-07-11 21:10:40 +0800

    欄位與方法、認識靜態 API

---
+++ 2DRun/Assets/Scripts/Learn1_Static.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Learn1_Static : MonoBehaviour
+{
+    private void Start()
+    {
+        // 靜態成員用法
+        // 靜態屬性 Property = 欄位 Field (儲存資料)
+        // 語法：類別名稱.靜態屬性名稱
+        print(Mathf.PI);
+        print(Mathf.Infinity);
+
+        print(Random.value);     // 0 - 1 隨機值
+
+        // 取得 100 - 500 隨機值
+        print(Mathf.Abs(-999));
+    }
+
+    private void Update()
+    {
+        // print(Time.time);
+    }
+}
---
+++ 2DRun/Assets/Scripts/Player.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位區域
+    /* 說明區域
+    // 命名規則
+    // 1. 用有意義的名稱
+    // 2. 不要使用數字開頭
+    // 3. 不要使用特殊符號包含：@#/* 空格
+    // 4. 可以使用中文 (不建議)
+
+    // 欄位語法
+    // 類型 欄位名稱 = 值;
+    // 沒有 = 值
+    // 整數、浮點數 預設值 0
+    // 字串 預設值 ""
+    // 布林值 fakse
+
+    // 私人 private - 僅限於此類別使用 | 不會顯示 - 預設值
+    // 公開 public - 所有類別皆可使用 | 會顯示
+
+    // 欄位屬性 [屬性名稱()]
+    // 標題 Header
+    // 提示 ToolTip
+    // 範圍 Range
+    */
+
+    [Header("速度"), Tooltip("角色的移動速度"), Range(10, 1500)]
+    public int speed = 50;
+    [Header("血量"), Tooltip("這是角色的血量~")]
+    public float hp = 999.9f;
+    [Header("金幣數量"),Tooltip("儲存角色吃了幾顆金幣")]
+    public int coin;
+    [Header("跳躍高度"), Range(100, 2000)]
+    public int height = 500;
+    [Header("音效區域")]
+    public AudioClip soundJump;
+    public AudioClip soundSlide;
+    public AudioClip soundHit;
+    [Header("角色是否死亡"), Tooltip("True 代表死亡，False 代表尚未死亡")]
+    public bool dead;
+
+
+    #endregion
+
+    #region 方法區域
+    // C# 括號符號都是成對出現的：() [] {} "" ''
+    // 摘要：方法的說明
+    // 在方法上方添加三條 /
+    // 自訂方法 - 不會執行的，需要呼叫
+    // API - 功能倉庫
+    // 輸出功能 print("字串")
+
+    /// <summary>
+    /// 角色的跳躍功能：跳躍動畫、播放音效與往上跳
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
+    }
+
+    /// <summary>
+    /// 角色的滑行功能：滑行動畫、播放音效、縮小碰撞範圍
+    /// </summary>
+    private void Slide()
+    {
+        print("滑行");
+    }
+
+    /// <summary>
+    /// 碰到障礙物時受傷：扣寫
+    /// </summary>
+    private void Hit()
+    {
+        
+    }
+
+    /// <summary>
+    /// 吃金幣：金幣數量增加、更新介面、金幣音效
+    /// </summary>
+    private void EatCoin()
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡：動畫、遊戲結束
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+    #endregion
+
+    #region 事件區域
+    // 開始 Start
+    // 播放遊戲時執行一次
+    //初始化：
+    private void Start()
+    {
+        // 呼叫跳躍方法
+        Jump();
+    }
+    // 更新 Update
+    // 播放遊戲後一秒執行約 60 次 - 60FPS
+    // 移動、監聽玩家鍵盤、滑鼠與觸控
+    private void Update()
+    {
+        Slide();
+    }
+    #endregion
+}
